b5b0100d4299536ed1e1e72123cdd1513110789a
guillermo <guillermo18zgordon@gmail.com>
2023-07-05 23:01:04 -0500

    Calulos de rentabiliad neta y rentabilidad operativa
    APenas se saco los calulos de rentabilidad neta y operativa

--- src/views/admin/tables/components/ExelCard.js
+++ src/views/admin/tables/components/ExelCard.js
@@ -4,7 +4,11 @@ import * as XLSX from "xlsx";
 const ExcelCard = (props) => {
   const [tables, setTables] = useState([]);
   const [activeSheet, setActiveSheet] = useState("");
-
+  const [column8Sum, setColumn8Sum] = useState(0);
+  const [column8SumSelectedSheet, setColumn8SumSelectedSheet] = useState(0);
+  const [column10SumSelectedSheet, setColumn10SumSelectedSheet] = useState(0);
+  const [column7SumSelectedSheet, setColumn7SumSelectedSheet] = useState(0);
+  const [RentabiliadNeta, setRentabilidadNeta] = useState(0);
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -27,9 +31,20 @@ const ExcelCard = (props) => {
         tableData.push(table);
       });
 
-      // onJsonData(tableData);
       setTables(tableData);
       setActiveSheet(tableData[0]?.sheetName || "");
+
+      // Calcular la suma de la columna 8
+      let sum = 0;
+      tableData.forEach((table) => {
+        table.data.slice(1).forEach((row) => {
+          const cellValue = parseFloat(row[6]); // Columna 8 (el índice es 7 ya que los índices comienzan desde 0)
+          if (!isNaN(cellValue)) {
+            sum += cellValue;
+          }
+        });
+      });
+      setColumn8Sum(sum);
     };
 
     reader.readAsArrayBuffer(file);
@@ -37,8 +52,44 @@ const ExcelCard = (props) => {
 
   const handleSheetButtonClick = (sheetName) => {
     setActiveSheet(sheetName);
-  };
+    const selectedTable = tables.find((table) => table.sheetName === sheetName);
+    if (selectedTable) {
+      let sumColumn8 = 0;
+      let sumColumn10 = 0;
+      let sumColumn7 = 0; // Nueva variable para la suma de la columna 7 en la hoja "Compras 2022"
 
+      selectedTable.data.slice(1).forEach((row) => {
+        const cellValueColumn8 = parseFloat(row[7]); // Columna 8 (el índice es 7 ya que los índices comienzan desde 0)
+        const cellValueColumn10 = parseFloat(row[9]); // Columna 10 (el índice es 9)
+
+        if (!isNaN(cellValueColumn8)) {
+          sumColumn8 += cellValueColumn8;
+        }
+        if (!isNaN(cellValueColumn10)) {
+          sumColumn10 += cellValueColumn10;
+        }
+      });
+
+      // Buscar la hoja "Compras 2022" y sumar los valores de la columna 4
+      const compras2022Table = tables.find(
+        (table) => table.sheetName === "Compras 2022"
+      );
+      if (compras2022Table) {
+        compras2022Table.data.slice(1).forEach((row) => {
+          const cellValueColumn4 = parseFloat(row[4]); // Columna 4 (el índice es 3)
+          if (!isNaN(cellValueColumn4)) {
+            sumColumn7 += cellValueColumn4;
+          }
+        });
+      }
+
+      setColumn8SumSelectedSheet(sumColumn8);
+      setColumn10SumSelectedSheet(sumColumn10);
+      setColumn7SumSelectedSheet(sumColumn7); // Agregar la suma de la columna 4 en "Compras 2022" a la variable de estado
+
+      setRentabilidadNeta(sumColumn8 + sumColumn10);
+    }
+  };
   return (
     <div className="card">
       <div className="card-header">Importar archivo Excel</div>
@@ -61,6 +112,25 @@ const ExcelCard = (props) => {
               display: activeSheet === table.sheetName ? "block" : "none",
             }}
           >
+            <h3>Suma de la columna 8: {column8Sum}</h3>
+            {activeSheet && (
+              <h3>
+                Suma de la columna 8 (hoja {activeSheet}):{" "}
+                {column8SumSelectedSheet}
+              </h3>
+            )}
+            {activeSheet && (
+              <h3>
+                Suma de la columna 10 (hoja {activeSheet}):{" "}
+                {column10SumSelectedSheet}
+              </h3>
+            )}
+            {activeSheet && (
+              <h3>
+                Suma de la columna 7 (hoja {activeSheet}):{" "}
+                {column7SumSelectedSheet}
+              </h3>
+            )}
             <h3>{table.sheetName}</h3>
             <table className="table">
               <thead>
@@ -83,6 +153,7 @@ const ExcelCard = (props) => {
             </table>
           </div>
         ))}
+        <div></div>
       </div>
     </div>
   );
