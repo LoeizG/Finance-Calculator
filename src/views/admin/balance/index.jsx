24566fcb162f7dede38763ad9f489818583aa90c
Loeiz07 <loeiz.gonzalez@gmail.com>
2023-07-12 16:04:49 -0500

    v4

--- src/views/admin/balance/index.jsx
+++ src/views/admin/balance/index.jsx
@@ -28,13 +28,13 @@ const Tables = () => {
 
   return (
     <div>
-      <div className="mt-5 grid h-full grid-cols-1 gap-5 md:grid-cols-2">
+      {/*<div className="mt-5 grid h-full grid-cols-1 gap-5 md:grid-cols-2">
         <ExcelCard
           columnsData={Vista}
 
           // tableData={tableDataColumns}
         />
-      </div>
+  </div>*/}
 
       <div className="grid grid-cols-1 gap-5 mt-5 h-full md:grid-cols-1">
         <Activos
